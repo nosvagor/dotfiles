cf2b3db6b12bb8e3abdbf1d88bdc35b4ac210f0d
nosvagor <cullyn.newman@gmail.com>
2021-09-01 10:16:21 -0700

    rofi adjustments, good enough for now

---
+++ scripts/layouts.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+rofi_command="rofi -theme ~/.config/rofi/layouts.rasi"
+
+#Options
+notes="notes"
+dotfiles="dotfiles"
+golang="golang"
+calc="calculus"
+javascript="javascript"
+bash="bash"
+python="python"
+linear="linear"
+pandas="pandas"
+solidity="solidity"
+c="c++"
+stat="statistics"
+
+# Variable passed to rofi
+options="$golang\n$solidity\n$javascript\n$bash\n$c\n$python\n\
+$notes\n$dotfiles\n$calc\n$linear\n$pandas\n$stat"
+
+chosen="$(echo -e "$options" | $rofi_command -p "" -dmenu)"
+case $chosen in
+    $notes)
+		./scripts/basic-launch.sh notes
+        ;;
+    $dotfiles)
+		./scripts/basic-launch.sh dotfiles
+        ;;
+    $golang)
+		./scripts/code-launch.sh golang
+        ;;
+    $c)
+		./scripts/code-launch.sh c++
+        ;;
+    $bash)
+        ./scripts/code-launch.sh bash
+        ;;
+    $calc)
+        ./scripts/tex-launch.sh python/calculus calculus
+        ;;
+    $solidity)
+        ./scripts/code-launch.sh solidity
+        ;;
+    $python)
+        ./scripts/code-launch.sh python
+        ;;
+    $javascript)
+        ./scripts/code-launch.sh javascript
+        ;;
+    $pandas)
+		./scripts/code-launch.sh python/pandas
+        ;;
+    $linear)
+		./scripts/code-launch.sh python/linear
+        ;;
+    $stat)
+		./scripts/code-launch.sh python/statistics
+        ;;
+esac
