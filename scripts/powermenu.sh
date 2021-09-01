0aab999e0bf97f5b8436a767dde6b66f4aa1a959
nosvagor <cullyn.newman@gmail.com>
2021-09-01 09:27:29 -0700

    fixed rofi issue

---
+++ scripts/powermenu.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env bash
+
+uptime=$(uptime -p | sed -e 's/up //g')
+rofi_command="rofi -theme ~/.config/rofi/powermenu.rasi"
+
+# Options
+shutdown="襤"
+reboot="累"
+lock=""
+
+# Variable passed to rofi
+options="$lock\n$shutdown\n$reboot"
+
+chosen="$(echo -e "$options" | $rofi_command -p "$uptime" -dmenu -selected-row 1)"
+case $chosen in
+    $shutdown)
+		systemctl poweroff -i
+        ;;
+    $reboot)
+		systemctl reboot
+        ;;
+    $lock)
+		env XSECURELOCK_SAVER=saver_mplayer XSECURELOCK_LIST_VIDEOS_COMMAND="find ~/resources/videos -type f" XSECURELOCK_SHOW_HOSTNAME=0 xsecurelock
+        ;;
+esac
