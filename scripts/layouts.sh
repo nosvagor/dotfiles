2c742f49ee2bac4db457dd77beacccb51f58e88c
nosvagor <cullyn.newman@gmail.com>
2021-09-25 12:10:34 -0700

    upated layouts for destkop

--- scripts/layouts.sh
+++ scripts/layouts.sh
@@ -12,6 +12,8 @@ else
   size=phat
 fi
 
+echo $size
+
 begin () {
   bspc config focus_follows_pointer false
   bspc desktop -l tiled
@@ -46,12 +48,57 @@ basic_smol () {
   esac
 
   bspc node -f west.local
+  bspc config split_ratio 0.547
+}
+
+basic_phat () {
+
+  basic () {
+    bspc config split_ratio 0.3015
+    alacritty -e cava & sleep 0.5
+    bspc config split_ratio 0.894
+    bspc node -p north
+  }
+
+  case $1 in
+    dotfiles|notes)
+      basic
+      firefox --new-window & sleep 1
+      ;;
+    cpp)
+      basic
+      firefox --new-window https://github.com/nosvagor/notes/tree/main/water/$1 \
+      & sleep 1
+      ;;
+    linear)
+      basic
+      firefox --new-window https://github.com/nosvagor/notes/tree/main/fire/$1 \
+      & sleep 1
+      ;;
+    calculus)
+      bspc config split_ratio 0.3015
+      firefox --new-window https://calendar.google.com/calendar/u/0/r \
+      & sleep 1
+      bspc config split_ratio 0.642
+      bspc node -p east
+      zathura ~/notes/fire/$1/$1.pdf & sleep 0.5
+      bspc config split_ratio 0.777
+      bspc node -p south
+      alacritty -e cava & sleep 0.25
+      bspc node -f west
+      bspc config split_ratio 0.952
+      bspc node -p south
+      zathura & sleep 0.25
+      ;;
+  esac
+
+  bspc node -f west.local
+  bspc config split_ratio 0.3015
 }
 
 ###
 
 end () {
-  bspc config split_ratio 0.547
   bspc config focus_follows_pointer true
   notify-send -u low -t 3000 init $1 &
   exit 0
