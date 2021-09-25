2b5078083f04c8149ef99558af03cd6588bce038
nosvagor <cullyn.newman@gmail.com>
2021-09-25 08:50:33 -0700

    surface layouts done

--- scripts/layouts.sh
+++ scripts/layouts.sh
@@ -4,21 +4,81 @@ rofi_command="rofi -theme ~/.config/rofi/layouts.rasi"
 
 options=`echo "calculus linear cpp notes dotfiles\n" | tr ' ' '\n'`
 
+# smol = surface pro, phat = 3840 x 2160
+if xrandr | grep "eDP-1"
+then
+  size=smol
+else
+  size=phat
+fi
+
+begin () {
+  bspc config focus_follows_pointer false
+  bspc desktop -l tiled
+
+  alacritty & sleep 0.25
+}
+
+###
+
+basic_smol () {
+  bspc config split_ratio 0.547
+  bspc node -p east
+  alacritty -e cava & sleep 0.5
+
+  bspc config split_ratio 0.851
+  bspc node -p north
+  case $1 in
+    dotfiles|notes)
+      firefox --new-window & sleep 2
+      ;;
+    cpp)
+      firefox --new-window https://github.com/nosvagor/notes/tree/main/water/$1 \
+      & sleep 2
+      ;;
+    linear)
+      firefox --new-window https://github.com/nosvagor/notes/tree/main/fire/$1 \
+      & sleep 2
+      ;;
+    calculus)
+      zathura ~/notes/fire/calculus/$1.pdf & sleep 1
+      ;;
+  esac
+
+  bspc node -f west.local
+}
+
+###
+
+end () {
+  bspc config split_ratio 0.547
+  bspc config focus_follows_pointer true
+  notify-send -u low -t 3000 init $1 &
+  exit 0
+}
+
+open () {
+  begin
+  basic_$size $1
+  end
+}
+
+
 chosen="$(echo -e "$options" | $rofi_command -p "ﱖ" -dmenu)"
 case $chosen in
-    "notes")
-		./scripts/scripts/basic-launch.sh notes
-        ;;
-    "dotfiles")
-		./scripts/scripts/basic-launch.sh .dotfiles
-        ;;
-    "cpp")
-		./scripts/code-launch.sh water/cpp cpp
-        ;;
-    "calculus")
-        ./scripts/scripts/tex-launch.sh fire/calculus calculus
-        ;;
-    "linear")
-		./scripts/code-launch.sh fire/linear linear
-        ;;
+  notes)
+    open notes
+    ;;
+  dotfiles)
+    open dotfiles
+    ;;
+  cpp)
+    open cpp
+    ;;
+  calculus)
+    open calculus
+    ;;
+  linear)
+    open linear
+    ;;
 esac
--- scripts/wifi.sh
+++ scripts/wifi.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 rm -rf /var/run/wpa_supplicant/wlp1s0
-systemctl start bluetooth
 echo "stopping wpa_supplicant" "wifi off"
 wpa_supplicant -B -i wlp1s0 -c /etc/wpa_supplicant/$1.conf
 echo "conecting... psu" "wifi on"
