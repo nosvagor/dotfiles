0eb4c0b9d3e6fa97cbec97252add2e8563bdca79
nosvagor <cullyn.newman@gmail.com>
2021-12-16 12:22:09 -0800

    major refactor pending, minor update

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -2,14 +2,14 @@
 
 # TODO: do this better
 
-ln -sfn $HOME/.dotfiles/.Xresources $HOME/
-ln -sfn $HOME/.dotfiles/.xinitrc $HOME/
-ln -sfn $HOME/.dotfiles/.latexmkrc $HOME/
-ln -sfn $HOME/.dotfiles/.tmux-session $HOME/
-ln -sfn $HOME/.dotfiles/.zshrc $HOME/
-ln -sfn $HOME/.dotfiles/config/* $HOME/.config/
-ln -sfn $HOME/.dotfiles/resources $HOME/
-ln -sfn $HOME/.dotfiles/scripts $HOME/
+ln -sfn $HOME/dotfiles/.Xresources $HOME/
+ln -sfn $HOME/dotfiles/.xinitrc $HOME/
+ln -sfn $HOME/dotfiles/.latexmkrc $HOME/
+ln -sfn $HOME/dotfiles/.tmux-session $HOME/
+ln -sfn $HOME/dotfiles/.zshrc $HOME/
+ln -sfn $HOME/dotfiles/config/* $HOME/.config/
+ln -sfn $HOME/dotfiles/resources $HOME/
+ln -sfn $HOME/dotfiles/scripts $HOME/
 
 mkdir -pv $HOME/.local/bin/
 ln -sfn $HOME/.dotfiles/bin/* $HOME/.local/bin/
