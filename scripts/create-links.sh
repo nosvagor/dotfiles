4535dd19ef4e66e75e0452609ccc82e064dc447b
nosvagor <nosvagor@gmail.com>
2022-03-18 10:28:06 -0700

    scripts minor update

--- scripts/create-links.sh
+++ scripts/create-links.sh
@@ -4,6 +4,5 @@
 
 ln -sfn $HOME/dotfiles/home/.* $HOME/
 ln -sfn $HOME/dotfiles/config/* $HOME/.config/
-ln -sfn $HOME/dotfiles/scripts/* $HOME/.local/bin/
 
 ln -sfn $HOME/dotfiles/resources/page.css $HOME/.config/youtube-music-desktop-app/custom/css
