407ff0ae5c97cc62ee802632accf8ebb90652d40
nosvagor <nosvagor@gmail.com>
2022-07-12 20:48:07 +0000

    blank?

---
+++ cullyn/.config/nvim/init.lua
@@ -0,0 +1,27 @@
+local status_ok, impatient = pcall(require,"impatient")
+if not status_ok then
+	return
+end
+
+local config_files = {
+	"plugins",
+	"options",
+	"keymaps",
+	"cmp",
+	"colorscheme",
+	"lsp",
+	"telescope",
+	"treesitter",
+	"autopairs",
+	"comment",
+	"gitsigns",
+	"nvim-tree",
+	"toggleterm",
+	"lualine",
+	"alpha",
+	"spectre",
+}
+
+for _, file in ipairs(config_files) do
+	require("user." .. file)
+end
--- scripts/create-links.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# TODO: do this better
-
-ln -sfn $HOME/dotfiles/home/.* $HOME/
-ln -sfn $HOME/dotfiles/config/* $HOME/.config/
-
-ln -sfn $HOME/dotfiles/resources/page.css $HOME/.config/youtube-music-desktop-app/custom/css
