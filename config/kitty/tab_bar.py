a1b3792238bd7d1dabe08012f732128ce822aad9
nosvagor <nosvagor@gmail.com>
2023-01-25 12:55:33 -0800

    added old tab bar, not worth doing as of now

--- config/kitty/tab_bar.py
+++ config/kitty/tab_bar.py
@@ -1,111 +1,195 @@
-import datetime
-
-from kitty.fast_data_types import Screen
-from kitty.tab_bar import DrawData, ExtraData, TabBarData, as_rgb, draw_title
+from os import getlogin, uname
+from pathlib import Path
+from kitty.boss import get_boss
+from kitty.fast_data_types import Screen, get_options
 from kitty.utils import color_as_int
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    Formatter,
+    TabBarData,
+    as_rgb,
+    draw_attributed_string,
+    draw_title,
+)
 
-center: str = ""
+opts = get_options()  # --------------------------------------------⮯
+# --------------------------------------------------------------\
+# black  |  red     green    blue     magenta  cyan     white   | color
+# color0 |  color1  color2   color4   color5   color6   color7  | normal
+# color8 |  color9  color10  color12  color13  color14  color15 | bright
+# --------------------------------------------------------------/
+FG = as_rgb(color_as_int(opts.background))
+BG = as_rgb(color_as_int(opts.color4))
+ACCENT = as_rgb(color_as_int(opts.selection_background))
+BAR_BG = as_rgb(color_as_int(opts.tab_bar_background))
+ACTIVE_BG = as_rgb(color_as_int(opts.active_tab_background))
+SEPARATOR_SYMBOL, SOFT_SEPARATOR_SYMBOL = ("", "")
+SEPARATOR_SYMBOL_RIGHT = ""
+TRUNCATION_SYMBOL = "/⟜⊸/"
+ICON, ICON_HOST, ICON_USER, = (
+    "   ",
+    " 歷 ",
+    " ",
+)
+RIGHT_MARGIN = -6
+REFRESH_TIME = 1
+right_status_length = -1
 
-class DrawTab:
-    """"""
 
-    left_length: int = 0
-    center_length: int = 0
-    right_length: int = 0
+def _draw_icon(screen: Screen, index: int) -> int:
+    if index != 1:
+        return 0
+    fg, bg = screen.cursor.fg, screen.cursor.bg
+    screen.cursor.fg, screen.cursor.bg = FG, BG
+    screen.draw(ICON)
+    screen.cursor.fg, screen.cursor.bg = BG, bg
+    screen.draw(SEPARATOR_SYMBOL)
+    screen.cursor.fg, screen.cursor.bg = fg, bg
+    screen.cursor.x = len(ICON + SEPARATOR_SYMBOL)
+    return screen.cursor.x
 
-    def __init__(self) -> None:
-        self.right_length = len(self.get_datetime())
 
-    def get_datetime(self) -> str:
-        now = datetime.datetime.now()
-        date = now.strftime(" %Y/%m/%d %H:%M:%S")
-        return date
+def get_cwd():
+    cwd = ""
+    tab_manager = get_boss().active_tab_manager
+    if tab_manager is not None:
+        window = tab_manager.active_window
+        if window is not None:
+            cwd = window.cwd_of_child
 
-    def draw_left_status(self, screen: Screen, index: int) -> int:
-        text = f"index: {index}"
-        screen.draw(text)
-        screen.cursor.x = len(text)
-        self.left_length = len(text)
-        return screen.cursor.x
-
-    def draw_center_status(
-        self,
-        draw_data: DrawData,
-        screen: Screen,
-        tab: TabBarData,
-        before: int,
-        max_title_length: int,
-        index: int,
-        is_last: bool,
-        extra_data: ExtraData,
-    ) -> int:
-        global center
-        if center:
-            center = center + " ┇ " + tab.title
+    cwd_parts = list(Path(cwd).parts)
+    if len(cwd_parts) > 1:
+        if cwd_parts[1] == "home":
+            cwd_parts[0] = "  "
+            cwd_parts[1:3] = []
         else:
-            center = tab.title
-        self.center_length = len(center)
+            cwd_parts[0] = "  "
+    else:
+        cwd_parts[0] = "  "
 
-        draw_spaces = screen.columns - screen.cursor.x - self.center_length
-        if draw_spaces < 0:
-            screen.draw(" " * draw_spaces)
-
-        screen.cursor.x = screen.columns // 2 - self.center_length // 2
-        screen.draw(center)
-        return screen.cursor.x
-
-    def draw_right_status(self, screen: Screen, is_last: bool) -> int:
-        """draw right status"""
-        end = screen.cursor.x
-        if not is_last:
-            return end
-
-        draw_spaces = screen.columns - screen.cursor.x - self.right_length
-        if draw_spaces > 0:
-            screen.draw(" " * draw_spaces)
-        screen.draw(self.get_datetime())
-        if screen.columns - screen.cursor.x > self.right_length:
-            screen.cursor.x = screen.columns - self.right_length
-        return end
-
-    def draw_tab(
-        self,
-        draw_data: DrawData,
-        screen: Screen,
-        tab: TabBarData,
-        before: int,
-        max_title_length: int,
-        index: int,
-        is_last: bool,
-        extra_data: ExtraData,
-    ) -> int:
-        """"""
-        global center
-        if index == 1:
-            center = ""
-            self.draw_left_status(screen, index)
-
-        self.draw_center_status(
-            draw_data,
-            screen,
-            tab,
-            before,
-            max_title_length,
-            index,
-            is_last,
-            extra_data,
+    if len(cwd_parts) < 5:
+        cwd = cwd_parts[0] + "/".join(cwd_parts[1:])
+    else:
+        cwd = (
+            cwd_parts[0]
+            + "/".join(cwd_parts[1:2])
+            + TRUNCATION_SYMBOL
+            + "/".join(cwd_parts[-2:])
         )
 
-        if screen.cursor.x - (screen.columns - self.left_length - self.right_length - self.center_length) > 0:
-            return screen.cursor.x
+    return cwd
 
-        self.draw_right_status(
-            screen,
-            is_last,
-        )
 
+def _draw_cwd(screen: Screen, index: int) -> int:
+    if index != 1:
+        return 0
+    fg, bg = screen.cursor.fg, screen.cursor.bg
+    screen.cursor.fg, screen.cursor.bg = BG, ACTIVE_BG
+    cwd = get_cwd()
+    screen.draw(cwd)
+    screen.cursor.fg, screen.cursor.bg = ACTIVE_BG, BAR_BG
+    screen.draw(SEPARATOR_SYMBOL)
+    screen.draw(" |-> ")
+    screen.cursor.fg, screen.cursor.bg = fg, bg
+    screen.cursor.x = len(cwd) + 11
+    return screen.cursor.x
+
+
+def _draw_left_status(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    index: int,
+    extra_data: ExtraData,
+) -> int:
+    if screen.cursor.x >= screen.columns - right_status_length:
         return screen.cursor.x
+    tab_bg, tab_fg = screen.cursor.bg, screen.cursor.fg
+    if index == 1:
+        screen.cursor.fg, screen.cursor.bg = tab_bg, BAR_BG
+        screen.draw(SEPARATOR_SYMBOL_RIGHT)
+        screen.cursor.bg = tab_bg
+    default_bg = as_rgb(int(draw_data.default_bg))
+    if extra_data.next_tab:
+        next_tab_bg = as_rgb(draw_data.tab_bg(extra_data.next_tab))
+        needs_soft_separator = next_tab_bg == tab_bg
+    else:
+        next_tab_bg = default_bg
+        needs_soft_separator = False
+    if screen.cursor.x <= len(ICON):
+        screen.cursor.x = len(ICON)
+    screen.draw(" ")
+    screen.cursor.bg = tab_bg
+    draw_title(draw_data, screen, tab, index)
+    if not needs_soft_separator:
+        screen.draw(" ")
+        screen.cursor.fg = tab_bg
+        screen.cursor.bg = next_tab_bg
+        screen.draw(SEPARATOR_SYMBOL)
+    else:
+        prev_fg = screen.cursor.fg
+        if tab_bg == tab_fg:
+            screen.cursor.fg = default_bg
+        elif tab_bg != default_bg:
+            c1 = draw_data.inactive_bg.contrast(draw_data.default_bg)
+            c2 = draw_data.inactive_bg.contrast(draw_data.inactive_fg)
+            if c1 < c2:
+                screen.cursor.fg = default_bg
+        screen.draw(" " + SOFT_SEPARATOR_SYMBOL)
+        screen.cursor.fg = prev_fg
+    end = screen.cursor.x
+    return end
 
 
-def draw_tab(*args) -> int:
-    return DrawTab().draw_tab(*args)
+def _draw_right_status(screen: Screen, is_last: bool, cells: list) -> int:
+    if not is_last:
+        screen.cursor.bg = FG
+        return 0
+    draw_attributed_string(Formatter.reset, screen)
+    screen.cursor.x = screen.columns - right_status_length
+    screen.cursor.fg = 0
+    screen.cursor.bg = 0
+    for color_fg, color_bg, status in cells:
+        screen.cursor.fg = color_fg
+        screen.cursor.bg = color_bg
+        screen.draw(status)
+    return screen.cursor.x
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,  # Not accessed, but things break without it?
+    max_title_length: int,  # Not accessed, but things break without it?
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    global right_status_length
+    app = ICON_USER + getlogin() + " " + SEPARATOR_SYMBOL_RIGHT
+    host = uname()[1] + ICON_HOST
+    cells = []
+    cells.append((ACTIVE_BG, BAR_BG, SEPARATOR_SYMBOL_RIGHT))
+    cells.append((BG, ACTIVE_BG, app))
+    cells.append((FG, BG, host))
+    right_status_length = RIGHT_MARGIN + 1
+    for cell in cells:
+        right_status_length += len(str(cell[1]))
+
+    _draw_icon(screen, index)
+    _draw_cwd(screen, index)
+    _draw_left_status(
+        draw_data,
+        screen,
+        tab,
+        index,
+        extra_data,
+    )
+    _draw_right_status(
+        screen,
+        is_last,
+        cells,
+    )
+    return screen.cursor.x
