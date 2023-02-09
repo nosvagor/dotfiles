d8199c731dd1e980107326a9f61fb16be29521a4
nosvagor <nosvagor@gmail.com>
2023-02-08 21:29:24 -0800

    xplr config progress, updating default config layout

--- config/xplr/init.lua
+++ config/xplr/init.lua
@@ -5,22 +5,81 @@ local xplr = xplr -- The globally exposed configuration to be overridden.
 
 -- 🌐 General: ⮯ {{{
 
-local function styled(color, mods)
-    return { fg = color or nil, add_modifiers = mods or nil }
+local orange = {}
+-- https://xplr.dev/en/style (shorthand ⮯)
+local function style(fg, add_mods)
+	return {
+		fg = fg or nil,
+		add_modifiers = add_mods or nil,
+	}
 end
 
-local function format(fmt, style)
-    return {
-        format = fmt or nil,
-        style = style or nil
-    }
+-- more shorthand for common {format, style} pattern
+local function format(fmt, color)
+	return {
+		format = fmt or nil,
+		style = { fg = color } or {},
+	}
 end
 
 -- https://github.com/sayanarijit/xplr/blob/main/src/init.lua
 local general = {
+	disable_debug_error_mode = true,
 	enable_mouse = true,
 	show_hidden = false,
-	prompt = format("  ", styled("Blue")),
+	read_only = false,
+	enable_recover_mode = false,
+	hide_remaps_in_help_menu = false,
+	enforce_bounded_index_navigation = false,
+	prompt = format("  ", "Blue"),
+	logs = {
+		success = format("SUCCESS", "Green"),
+		warning = format("WARNING", "Yellow"),
+		error = format("ERROR", "Red"),
+	},
+	table = {
+		header = {
+			cols = {
+				{ format = "  " },
+				{ format = "╭──⨒ path" },
+				{ format = "⼈⾕⾥" },
+				{ format = "猪size ─╮" },
+				{ format = " modified" },
+			},
+			style = { fg = "Blue" },
+			height = 1,
+		},
+		row = {
+			cols = {
+				{ format = "builtin.fmt_general_table_row_cols_0" },
+				{ format = "builtin.fmt_general_table_row_cols_1" },
+				{ format = "builtin.fmt_general_table_row_cols_2" },
+				{ format = "builtin.fmt_general_table_row_cols_3" },
+				{ format = "builtin.fmt_general_table_row_cols_4" },
+			},
+			style = {},
+			height = 0,
+		},
+		style = {},
+		tree = {
+			{ format = "├", style = {} },
+			{ format = "├", style = {} },
+			{ format = "╰", style = {} },
+		},
+		col_spacing = 1,
+		col_widths = {
+			{ Percentage = 5 },
+			{ Percentage = 55 },
+			{ Percentage = 5 },
+			{ Percentage = 15 },
+			{ Percentage = 20 },
+		},
+	},
+	default_ui = {
+		prefix = " ",
+		suffix = "",
+		style = {},
+	},
 	focus_ui = {
 		prefix = "⌬ ╼( ",
 		suffix = " )╾",
@@ -31,70 +90,115 @@ local general = {
 	selection_ui = {
 		prefix = "⟪ ",
 		suffix = " ⟫",
-        style = styled("Blue", { "Dim" }),
+		style = style("Cyan", { "Bold" }),
 	},
 	focus_selection_ui = {
-		prefix = " [",
-		suffix = "]",
-		style = styled("Blue", { "Bold" }),
+		prefix = " ╼[",
+		suffix = "]╾",
+		style = {
+			fg = {
+				Rgb = { 245, 169, 127 },
+			},
+			add_modifiers = { "Bold" },
+		},
 	},
 	sort_and_filter_ui = {
-		separator = format("  ", styled("DarkGray")),
+		separator = format("  ", "DarkGray"),
 		sort_direction_identifiers = {
-			forward = format("⮯", styled("Blue")),
-			reverse = format("⮭", styled("Red")),
+			forward = format("⮯", "Blue"),
+			reverse = format("⮭", "Red"),
 		},
 		sorter_identifiers = {
-			ByExtension = format(".", styled("Green")),
-			ByICanonicalAbsolutePath = format("⨓", styled("Yellow")),
-			ByIRelativePath = format("∫", styled("White")),
-			ByISymlinkAbsolutePath = format("", styled("Megenta")),
-			ByIsBroken = format("", styled("")),
-			ByIsDir = format("", styled("")),
-			ByIsFile = format("", styled("")),
-			ByIsReadonly = format("", styled("")),
-			ByIsSymlink = format("", styled("")),
-			ByMimeEssence = format("", styled("")),
-			ByRelativePath = format("", styled("")),
-			BySize = format("", styled("")),
-			ByCreated = format("", styled("")),
-			ByLastModified = format("", styled("")),
-			ByCanonicalAbsolutePath = format("", styled("")),
-			ByCanonicalExtension = format("", styled("")),
-			ByCanonicalIsDir = format("", styled("")),
-			ByCanonicalIsFile = format("", styled("")),
-			ByCanonicalIsReadonly = format("", styled("")),
-			ByCanonicalMimeEssence = format("", styled("")),
-			ByCanonicalSize = format("", styled("")),
-			ByCanonicalCreated = format("", styled("")),
-			ByCanonicalLastModified = format("", styled("")),
-			BySymlinkAbsolutePath = format("", styled("")),
-			BySymlinkExtension = format("", styled("")),
-			BySymlinkIsDir = format("", styled("")),
-			BySymlinkIsFile = format("", styled("")),
-			BySymlinkIsReadonly = format("", styled("")),
-			BySymlinkMimeEssence = format("", styled("")),
-			BySymlinkSize = format("", styled("")),
-			BySymlinkCreated = format("", styled("")),
-			BySymlinkLastModified = format("", styled("")),
+			ByExtension = { format = "ext", style = {} },
+			ByICanonicalAbsolutePath = { format = "[ci]abs", style = {} },
+			ByIRelativePath = { format = "[i]rel", style = {} },
+			ByISymlinkAbsolutePath = { format = "[si]abs", style = {} },
+			ByIsBroken = { format = "⨯", style = {} },
+			ByIsDir = { format = "dir", style = {} },
+			ByIsFile = { format = "file", style = {} },
+			ByIsReadonly = { format = "ro", style = {} },
+			ByIsSymlink = { format = "sym", style = {} },
+			ByMimeEssence = { format = "mime", style = {} },
+			ByRelativePath = { format = "rel", style = {} },
+			BySize = { format = "size", style = {} },
+			ByCreated = { format = "created", style = {} },
+			ByLastModified = { format = "modified", style = {} },
+
+			ByCanonicalAbsolutePath = { format = "[c]abs", style = {} },
+			ByCanonicalExtension = { format = "[c]ext", style = {} },
+			ByCanonicalIsDir = { format = "[c]dir", style = {} },
+			ByCanonicalIsFile = { format = "[c]file", style = {} },
+			ByCanonicalIsReadonly = { format = "[c]ro", style = {} },
+			ByCanonicalMimeEssence = { format = "[c]mime", style = {} },
+			ByCanonicalSize = { format = "[c]size", style = {} },
+			ByCanonicalCreated = { format = "[c]created", style = {} },
+			ByCanonicalLastModified = { format = "[c]modified", style = {} },
+
+			BySymlinkAbsolutePath = { format = "[s]abs", style = {} },
+			BySymlinkExtension = { format = "[s]ext", style = {} },
+			BySymlinkIsDir = { format = "[s]dir", style = {} },
+			BySymlinkIsFile = { format = "[s]file", style = {} },
+			BySymlinkIsReadonly = { format = "[s]ro", style = {} },
+			BySymlinkMimeEssence = { format = "[s]mime", style = {} },
+			BySymlinkSize = { format = "[s]size", style = {} },
+			BySymlinkCreated = { format = "[s]created", style = {} },
+			BySymlinkLastModified = { format = "[s]modified", style = {} },
+		},
+		filter_identifiers = {
+			RelativePathDoesContain = { format = "rel=~", style = {} },
+			RelativePathDoesEndWith = { format = "rel=$", style = {} },
+			RelativePathDoesNotContain = { format = "rel!~", style = {} },
+			RelativePathDoesNotEndWith = { format = "rel!$", style = {} },
+			RelativePathDoesNotStartWith = { format = "rel!^", style = {} },
+			RelativePathDoesStartWith = { format = "rel=^", style = {} },
+			RelativePathIs = { format = "rel==", style = {} },
+			RelativePathIsNot = { format = "rel!=", style = {} },
+			RelativePathDoesMatchRegex = { format = "rel=/", style = {} },
+			RelativePathDoesNotMatchRegex = { format = "rel!/", style = {} },
+
+			IRelativePathDoesContain = { format = "[i]rel=~", style = {} },
+			IRelativePathDoesEndWith = { format = "[i]rel=$", style = {} },
+			IRelativePathDoesNotContain = { format = "[i]rel!~", style = {} },
+			IRelativePathDoesNotEndWith = { format = "[i]rel!$", style = {} },
+			IRelativePathDoesNotStartWith = { format = "[i]rel!^", style = {} },
+			IRelativePathDoesStartWith = { format = "[i]rel=^", style = {} },
+			IRelativePathIs = { format = "[i]rel==", style = {} },
+			IRelativePathIsNot = { format = "[i]rel!=", style = {} },
+			IRelativePathDoesMatchRegex = { format = "[i]rel=/", style = {} },
+			IRelativePathDoesNotMatchRegex = { format = "[i]rel!/", style = {} },
+
+			AbsolutePathDoesContain = { format = "abs=~", style = {} },
+			AbsolutePathDoesEndWith = { format = "abs=$", style = {} },
+			AbsolutePathDoesNotContain = { format = "abs!~", style = {} },
+			AbsolutePathDoesNotEndWith = { format = "abs!$", style = {} },
+			AbsolutePathDoesNotStartWith = { format = "abs!^", style = {} },
+			AbsolutePathDoesStartWith = { format = "abs=^", style = {} },
+			AbsolutePathIs = { format = "abs==", style = {} },
+			AbsolutePathIsNot = { format = "abs!=", style = {} },
+			AbsolutePathDoesMatchRegex = { format = "abs=/", style = {} },
+			AbsolutePathDoesNotMatchRegex = { format = "abs!/", style = {} },
+
+			IAbsolutePathDoesContain = { format = "[i]abs=~", style = {} },
+			IAbsolutePathDoesEndWith = { format = "[i]abs=$", style = {} },
+			IAbsolutePathDoesNotContain = { format = "[i]abs!~", style = {} },
+			IAbsolutePathDoesNotEndWith = { format = "[i]abs!$", style = {} },
+			IAbsolutePathDoesNotStartWith = { format = "[i]abs!^", style = {} },
+			IAbsolutePathDoesStartWith = { format = "[i]abs=^", style = {} },
+			IAbsolutePathIs = { format = "[i]abs==", style = {} },
+			IAbsolutePathIsNot = { format = "[i]abs!=", style = {} },
+			IAbsolutePathDoesMatchRegex = { format = "[i]abs=/", style = {} },
+			IAbsolutePathDoesNotMatchRegex = { format = "[i]abs!/", style = {} },
+		},
+		search_identifier = {
+			format = "  ",
+			style = { fg = { Rgb = { 245, 169, 127 } } },
 		},
 	},
-}
-
-xplr.config.general.table.header.cols = {
-	{ format = "  ", style = {} },
-	{ format = "╭⨒ path", style = {} },
-	{ format = "usgrot", style = {} },
-	{ format = "猪size ─╮", style = {} },
-	{ format = " modified", style = {} },
-}
-
-xplr.config.general.table.col_widths = {
-	{ Percentage = 5 },
-	{ Percentage = 55 },
-	{ Percentage = 5 },
-	{ Percentage = 15 },
-	{ Percentage = 20 },
+	-- panel_ui = {
+	-- 	default = {
+	-- 		title = format(nil),
+	-- 	},
+	-- },
 }
 
 -- 🧮 ZHU LI, DO THE THING!
