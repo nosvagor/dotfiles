713237fd7546dc2831edbb507adf7b5ce550999a
nosvagor <nosvagor@gmail.com>
2023-03-04 14:52:47 -0800

    minor updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,5 @@
 -- ============================================================================
--- 📦 🢢 🢢 🢢  PACKER, PREP THE THING! 🢢 🢢 🢢 {{{
+-- 📦 PACKER, PREP THE THING! {{{
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -151,7 +151,7 @@ require("packer").startup(function(use)
 	-- }}}
 
 -- ============================================================================
--- 📦 🢣 🢣 🢣 PACKER, DO THE THING! 🢣 🢣 🢣  {{{
+-- 📦 PACKER, DO THE THING! {{{
 	use("wbthomason/packer.nvim")
 
 	if is_bootstrap then
