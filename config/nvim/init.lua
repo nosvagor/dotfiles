b209c402bf9f23f129d9d821bede69796c190486
nosvagor <nosvagor@gmail.com>
2023-03-20 10:43:08 -0700

    minor updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -325,6 +325,12 @@ require("lazy").setup({
 		build = function()
 			vim.fn["mkdp#util#install"]()
 		end,
+		config = function()
+			vim.g.mkdp_filetypes = { "markdown", "html" }
+            vim.g.mkdp_auto_start = 1
+            vim.g.mkdp_auto_close = 0
+            vim.g.mkdp_refresh_slow = 1
+		end,
 	},
 
 	{ "numToStr/Comment.nvim", opts = {} },
